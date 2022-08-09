0255dd84db9c833e64dd54fa3dc29dae3f7ad2fd
john <johnzhengyuliu@gmail.com>
2022-08-09 12:21:34 -0500

    mainnet test

--- pages/api/solpaytransfer.ts
+++ pages/api/solpaytransfer.ts
@@ -41,7 +41,7 @@ type ErrorOutput = {
 
 function get(res: NextApiResponse<MakeTransactionGetResponse>) {
   res.status(200).json({
-    label: "Use Promo",
+    label: "Get Promo",
     icon: "https://www.arweave.net/zdzBTTQW0V9jTyQWH_pE6D0xIKjnuxl9j9jtEs7Rk4g?ext=png",
   })
 }
